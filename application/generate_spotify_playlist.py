6e81506bfc6e36e102f1c025ca12d4e11eeb1476
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-02 14:05:48 +0100

    refactored google API auth

--- application/generate_spotify_playlist.py
+++ application/generate_spotify_playlist.py
@@ -1,13 +1,13 @@
-import json
 import os
 
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 import requests
 import spotipy
 import spotipy.util as util
 import youtube_dl
+from oauth2client import client, tools
+from oauth2client.file import Storage
 
 from resources.private_info import spotify_client_id, spotify_secret, spotify_username, spotify_redirect, spotify_scope
 
@@ -19,18 +19,15 @@ def get_youtube_client():
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "resources/client_secret.json"
-
-    # Get credentials and create an API client
     scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
 
-    # from the Youtube DATA API
-    youtube_client = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
-    return youtube_client
+    credential_path = os.path.join('./resources', 'google_credentials.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(client_secrets_file, scopes)
+        credentials = tools.run_flow(flow, store)
+    return googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
 
 
 def get_spotify_token():
@@ -88,13 +85,11 @@ class GenerateSpotifyPlaylist:
             artist = video["artist"]
 
             if song_name is not None and artist is not None:
-                # save all important info and skip any missing song and artist
                 self.all_song_info[video_title] = {
                     "youtube_url": youtube_url,
                     "song_name": song_name,
                     "artist": artist,
 
-                    # add the uri, easy to get song to put into playlist
                     "spotify_uri": self.get_spotify_uri(song_name, artist)
 
                 }
