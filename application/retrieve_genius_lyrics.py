6abdc7150201e27d4d89cd9117491e00ea52d815
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-04-03 15:25:31 +0100

    added functionality to obtain playback information for spotify to later pass to genius api

--- application/retrieve_genius_lyrics.py
+++ application/retrieve_genius_lyrics.py
@@ -1,4 +1,10 @@
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
 from generate_spotify_playlist import spotify_object
+from resources.private_info import genius_client_token
 
 
 # Method to obtain user information from spotify for welcome message
@@ -18,6 +24,18 @@ class RetrieveSongLyrics:
 
     def __init__(self):
         self.user = get_user_information()
+        self.playback_information = self.get_playback_information()
+
+    # Method to obtain current spotify playback information
+    def get_playback_information(self):
+        playback = spotify_object.currently_playing()
+        if playback is None:
+            raise TypeError("There is nothing currently playing. You have to play a song for this script to run.")
+            sys.exit()
+
+        artist_name = playback['item']['artists'][0]['name']
+        song_title = playback['item']['name']
+        return artist_name, song_title
 
 
 if __name__ == '__main__':
